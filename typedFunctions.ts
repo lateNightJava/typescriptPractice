065b89355927fcee57ec93501dda0b46145bfd82
lateNightJava <martinez.m31415@gmail.com>
2019-09-24 15:26:32 -0400

    Experimenting with jest and ts-jest

--- typedFunctions.ts
+++ typedFunctions.ts
@@ -5,7 +5,7 @@ interface SearchFunc {
 /* Different ways to write function signature */
 // const mySearch: SearchFunc = function(source: string, subString: string): boolean {
 // const mySearch: SearchFunc = function(src: string, sub: string): boolean {
-const mySearch: SearchFunc =  function(src, sub) {
+const mySearch: SearchFunc = function(src, sub) {
   const result = src.search(sub);
 
   // return "error"; // errors due to expecting return to be boolean
