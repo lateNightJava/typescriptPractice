66bf298e5cf7f7e6fbb7435050df6d77278dd7bc
lateNightJava <martinez.m31415@gmail.com>
2019-09-24 15:35:57 -0400

    stuff

--- add.test.ts
+++ add.test.ts
@@ -2,9 +2,6 @@ import { add } from './add';
 
 describe('Add Function', () => {
   it('Adds two numbers', () => {
-    // not safe when add(x: string, y: string): string
-    // passes test and does not properly type check
-    // expect(add(1,2)).toEqual(3);
-    expect(add(1,2)).toBe(3);
+    expect(add(1,2)).toEqual(3);
   });
 });
